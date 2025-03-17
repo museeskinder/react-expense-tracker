c1e09d89c3d5e1b76cf2a4e2bf44b62ec1854ed8
museeskinder <eskindermuse@gmail.com>
2025-03-17 12:14:31 +0300

    feat: introduced redux-persist all data will be persisteent to use

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
-import { store } from "../src/store/index.js";
+import { store, persistedStore } from "../src/store/index.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { expenseSlice } from "./expense/expense-slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
-
-export const store = configureStore({
-    reducer: {
-        EXPENSE: expenseSlice.reducer
-    }
+const combinedReducers = combineReducers({
+    EXPENSE: expenseSlice.reducer
 });
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducers = persistReducer(persistConfig, combinedReducers);
+
+const store = configureStore({
+    reducer: persistedReducers,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+const persistedStore = persistStore(store);
+
+export { store, persistedStore };
